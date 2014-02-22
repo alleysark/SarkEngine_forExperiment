dabe03483ff91d2c78c8598e57af704c46160980
yeramsim <yeram91@gmail.com>
2014-02-23 01:41:37 +0900

    vector4 member detailed
    to use x,y,z,w as r,g,b,a

--- SarkLibrary/types.h
+++ SarkLibrary/types.h
@@ -342,7 +342,14 @@ namespace sarklib{
 	*/
 	class Vector4{
 	public:
-		float x, y, z, w;
+		union{
+			struct{
+				float x, y, z, w;
+			};
+			struct{
+				float r, g, b, a;
+			};
+		};
 
 		Vector4() :x(0.0f), y(0.0f), z(0.0f), w(0.0f)
 		{ }
