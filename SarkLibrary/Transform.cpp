af92cc782804262d2a889550c568f322dc5cf97c
yeramsim <yeram91@gmail.com>
2014-03-11 19:12:34 +0900

    separate Transform.h
    into header and implementing file

---
+++ SarkLibrary/Transform.cpp
@@ -0,0 +1,61 @@
+#include "Transform.h"
+
+namespace sarklib{
+
+	Transform::Transform(){
+		mmatWorld.MakeIdentity();
+	}
+	Transform::~Transform(){}
+
+	// get transformation matrix
+	const Matrix4& Transform::GetMatrix(){
+		if (mmatWorld.m[3][3] != 0)
+			return mmatWorld;
+
+		mmatWorld = mRotator.ToMatrix4(true);
+		mmatWorld.m[0][3] = mPosition.x;
+		mmatWorld.m[1][3] = mPosition.y;
+		mmatWorld.m[2][3] = mPosition.z;
+		mmatWorld.m[3][3] = 1.0f;
+		return mmatWorld;
+	}
+
+	// get position vector
+	const Position3& Transform::GetPosition() const{
+		return mPosition;
+	}
+
+	// translate into the other position from given vector
+	void Transform::Translate(const Position3& position){
+		mPosition = position;
+		mmatWorld.m[3][3] = 0.0f;
+	}
+	// translate into the other position from given vector
+	void Transform::Translate(real x, real y, real z){
+		mPosition.Set(x, y, z);
+		mmatWorld.m[3][3] = 0.0f;
+	}
+
+	// translate this position additionally
+	void Transform::TranslateMore(const Position3& add){
+		mPosition += add;
+		mmatWorld.m[3][3] = 0.0f;
+	}
+	// translate this position additionally
+	void Transform::TranslateMore(real add_x, real add_y, real add_z){
+		mPosition += Position3(add_x, add_y, add_z);
+		mmatWorld.m[3][3] = 0.0f;
+	}
+
+	// rotate it from given axis and theta
+	void Transform::Rotate(const Vector3& axis, real theta){
+		mRotator.MakeRotatingQuat(axis, theta);
+		mmatWorld.m[3][3] = 0.0f;
+	}
+	// rotate it from given rotating factor roll(z-axis), pitch(x-axis) and yaw(y-axis)
+	void Transform::Rotate(real roll, real pitch, real yaw){
+		mRotator.MakeRotatingQuat(roll, pitch, yaw);
+		mmatWorld.m[3][3] = 0.0f;
+	}
+
+}
--- SarkLibrary/Transform.h
+++ SarkLibrary/Transform.h
@@ -13,61 +13,29 @@ namespace sarklib{
 		Matrix4 mmatWorld;
 
 	public:
-		Transform(){
-			mmatWorld.MakeIdentity();
-		}
-		~Transform(){}
+		Transform();
+		~Transform();
 
 		// get transformation matrix
-		const Matrix4& GetMatrix(){
-			if (mmatWorld.m[3][3] != 0)
-				return mmatWorld;
-
-			mmatWorld = mRotator.ToMatrix4(true);
-			mmatWorld.m[0][3] = mPosition.x;
-			mmatWorld.m[1][3] = mPosition.y;
-			mmatWorld.m[2][3] = mPosition.z;
-			mmatWorld.m[3][3] = 1.0f;
-			return mmatWorld;
-		}
+		const Matrix4& GetMatrix();
 
 		// get position vector
-		const Position3& GetPosition() const{
-			return mPosition;
-		}
+		const Position3& GetPosition() const;
 
 		// translate into the other position from given vector
-		void Translate(const Position3& position){
-			mPosition = position;
-			mmatWorld.m[3][3] = 0.0f;
-		}
+		void Translate(const Position3& position);
 		// translate into the other position from given vector
-		void Translate(real x, real y, real z){
-			mPosition.Set(x, y, z);
-			mmatWorld.m[3][3] = 0.0f;
-		}
+		void Translate(real x, real y, real z);
 
 		// translate this position additionally
-		void TranslateMore(const Position3& add){
-			mPosition += add;
-			mmatWorld.m[3][3] = 0.0f;
-		}
+		void TranslateMore(const Position3& add);
 		// translate this position additionally
-		void TranslateMore(real add_x, real add_y, real add_z){
-			mPosition += Position3(add_x, add_y, add_z);
-			mmatWorld.m[3][3] = 0.0f;
-		}
+		void TranslateMore(real add_x, real add_y, real add_z);
 
 		// rotate it from given axis and theta
-		void Rotate(const Vector3& axis, real theta){
-			mRotator.MakeRotatingQuat(axis, theta);
-			mmatWorld.m[3][3] = 0.0f;
-		}
+		void Rotate(const Vector3& axis, real theta);
 		// rotate it from given rotating factor roll(z-axis), pitch(x-axis) and yaw(y-axis)
-		void Rotate(real roll, real pitch, real yaw){
-			mRotator.MakeRotatingQuat(roll, pitch, yaw);
-			mmatWorld.m[3][3] = 0.0f;
-		}
+		void Rotate(real roll, real pitch, real yaw);
 	};
 
 }
